74226a9eef4009ce957561409f4955b0fce433d2
Ubuntu <ubuntu@ip-172-31-44-196.us-east-2.compute.internal>
2021-09-05 08:39:00 +0000

    Implement basic login

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,6 +11,16 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
   include ApplicationHelper
+  
+  # テストユーザーがログイン中の場合にtrueを返す
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
+  
+  # 現在のユーザーをログアウトする
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
 
-  # Add more helper methods to be used by all tests here...
 end
